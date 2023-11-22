88132dc8011390a3a8124322f797c6ecad7b761e
ivan.betanzos <ivan.betanzos89@gmail.com>
2023-11-22 21:24:17 +0100

    fix UserId to be a long value object

--- src/main/java/com/films/system/common/domain/valueobject/UserId.java
+++ src/main/java/com/films/system/common/domain/valueobject/UserId.java
@@ -1,9 +1,7 @@
 package com.films.system.common.domain.valueobject;
 
-import java.util.UUID;
-
-public class UserId extends BaseId<UUID> {
-    public UserId(UUID value) {
+public class UserId extends BaseId<Long> {
+    public UserId(Long value) {
         super(value);
     }
 }
